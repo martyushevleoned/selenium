390eb45add5e1d2870857ce9795e601862c85d71
martyushevleoned <56407820+martyushevleoned@users.noreply.github.com>
2020-09-25 20:53:28 +0500

    fix

--- main.py
+++ main.py
@@ -5,36 +5,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from time import sleep
 
+print('enter video name')
 request = input()
-# request = 'jerk ıt out cat'
-time = int(input())
-# time = 50
 
+print('enter time')
+time = int(input())
+
+# waiting
 while int(datetime.now().strftime("%H%M")) < time:
-    print('loading...')
+    print('waiting...')
     sleep(10)
 
-driver = webdriver.Firefox()
+# drivers
+# driver = webdriver.Firefox()
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome('yandexdriver.exe', options=webdriver.ChromeOptions())
+
+# find video
 wait = WebDriverWait(driver, 3)
-presence = EC.presence_of_element_located
-visible = EC.visibility_of_element_located
 driver.get('https://www.youtube.com/results?search_query={}'.format(request))
-wait.until(visible((By.ID, "video-title")))
+wait.until(EC.visibility_of_element_located((By.ID, "video-title")))
 driver.find_element_by_id("video-title").click()
 
 sleep(5)
-ad = driver.find_element_by_class_name('ytp-ad-text')
-try:
-    while ad.is_displayed() is True:
-        sleep(1)
-except Exception as e:
-    print('done')
 
+# full screen
 f = driver.find_element_by_class_name('ytp-fullscreen-button')
 f.click()
-
-timer = driver.find_element_by_class_name('ytp-play-progress')
-
-while True:
-    sleep(30)
-    timer.click()
+print('full screen')
